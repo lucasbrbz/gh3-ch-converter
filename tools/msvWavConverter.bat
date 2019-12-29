07d0ab835115d7400390e25d32bc1c4ca70b4481
Lucas <brbzmg22@gmail.com>
2019-12-29 01:50:05 -0300

    .wav conversion implemented and process fully automatizated

--- tools/msvWavConverter.bat
+++ tools/msvWavConverter.bat
@@ -1,196 +1,55 @@
 @echo off
-echo Splitting 1-1.IMF
-main.exe v 1 1
-cls
-echo Splitting 1-2.IMF
-main.exe v 1 2
-cls
-echo Splitting 1-3.IMF
-main.exe v 1 3
-cls
-echo Splitting 1-4.IMF
-main.exe v 1 4
-cls
-echo Splitting 1-5.IMF
-main.exe v 1 5
-cls
-echo Splitting 2-1.IMF
-main.exe v 2 1
-cls
-echo Splitting 2-2.IMF
-main.exe v 2 2
-cls
-echo Splitting 2-3.IMF
-main.exe v 2 3
-cls
-echo Splitting 2-4.IMF
-main.exe v 1 4
-cls
-echo Splitting 2-5.IMF
-main.exe v 2 5
-cls
-echo Splitting 3-1.IMF
-main.exe v 3 1
-cls
-echo Splitting 3-2.IMF
-main.exe v 3 2
-cls
-echo Splitting 3-3.IMF
-main.exe v 3 3
-cls
-echo Splitting 3-4.IMF
-main.exe v 3 4
-cls
-echo Splitting 3-5.IMF
-main.exe v 3 5
-cls
-echo Splitting 4-1.IMF
-main.exe v 4 1
-cls
-echo Splitting 4-2.IMF
-main.exe v 4 2
-cls
-echo Splitting 4-3.IMF
-main.exe v 4 3
-cls
-echo Splitting 4-4.IMF
-main.exe v 4 4
-cls
-echo Splitting 4-5.IMF
-main.exe v 4 5
-cls
-echo Splitting 5-1.IMF
-main.exe v 5 1
-cls
-echo Splitting 5-2.IMF
-main.exe v 5 2
-cls
-echo Splitting 5-3.IMF
-main.exe v 5 3
-cls
-echo Splitting 5-4.IMF
-main.exe v 5 4
-cls
-echo Splitting 5-5.IMF
-main.exe v 5 5
-cls
-echo Splitting 6-1.IMF
-main.exe v 6 1
-cls
-echo Splitting 6-2.IMF
-main.exe v 6 2
-cls
-echo Splitting 6-3.IMF
-main.exe v 6 3
-cls
-echo Splitting 6-4.IMF
-main.exe v 6 4
-cls
-echo Splitting 6-5.IMF
-main.exe v 6 5
-cls
-echo Splitting 7-1.IMF
-main.exe v 7 1
-cls
-echo Splitting 7-2.IMF
-main.exe v 7 2
-echo Success!
-cls
-echo Splitting 7-3.IMF
-main.exe v 7 3
-cls
-echo Splitting 7-4.IMF
-main.exe v 7 4
-cls
-echo Splitting 7-5.IMF
-main.exe v 7 5
-echo Success!
-cls
-echo Splitting 8-1.IMF
-main.exe v 8 1
-cls
-echo Splitting 8-2.IMF
-main.exe v 8 2
-cls
-echo Splitting 8-3.IMF
-main.exe v 8 3
-cls
-echo Splitting 8-4.IMF
-main.exe v 8 4
-cls
-echo Splitting b-1.IMF
-main.exe v b 1
-cls
-echo Splitting b-2.IMF
-main.exe v b 2
-cls
-echo Splitting b-3.IMF
-main.exe v b 3
-cls
-echo Splitting b-4.IMF
-main.exe v b 4
-cls
-echo Splitting b-5.IMF
-main.exe v b 5
-cls
-echo Splitting b-6.IMF
-main.exe v b 6
-cls
-echo Splitting b-7.IMF
-main.exe v b 7
-cls
-echo Splitting b-8.IMF
-main.exe v b 8
-cls
-echo Splitting b-9.IMF
-main.exe v b 9
-cls
-echo Splitting b-10.IMF
-main.exe v b 10
-cls
-echo Splitting b-11.IMF
-main.exe v b 11
-cls
-echo Splitting b-12.IMF
-main.exe v b 12
-cls
-echo Splitting b-13.IMF
-main.exe v b 13
-cls
-echo Splitting b-14.IMF
-main.exe v b 14
-cls
-echo Splitting b-15.IMF
-main.exe v b 15
-cls
-echo Splitting b-16.IMF
-main.exe v b 16
-cls
-echo Splitting b-17.IMF
-main.exe v b 17
-cls
-echo Splitting b-18.IMF
-main.exe v b 18
-cls
-echo Splitting b-19.IMF
-main.exe v b 19
-cls
-echo Splitting b-20.IMF
-main.exe v b 20
-cls
-echo Splitting b-21.IMF
-main.exe v b 21
-cls
-echo Splitting b-22.IMF
-main.exe v b 22
-cls
-echo Splitting b-23.IMF
-main.exe v b 23
-cls
-echo Splitting b-24.IMF
-main.exe v b 24
-cls
-echo Splitting b-25.IMF
-main.exe v b 25
-cls
+TITLE MSVWavConverter - Splitting MSV files
+
+FOR /L %%i IN (1,1,8) DO (
+	FOR /L %%j IN (1,1,5) DO (
+		IF %%i==8 (IF %%j==5 GOTO END_SPLIT)
+		echo Splitting %%i-%%j.IMF
+		audio-converter.exe v %%i %%j
+		CLS
+		echo Successfully split %%i-%%j.IMF
+		echo.
+		echo ------------------------------
+		echo.
+	)
+)
+
+:END_SPLIT
+
+echo All MSV files have been split.
+echo.
+echo ------------------------------
+echo.
+echo Initiating conversion to .wav...
+TIMEOUT 5
+CLS
+
+TITLE MSVWavConverter - Converting .vag files to .wav
+
+FOR /L %%i IN (1,1,8) DO (
+	FOR /L %%j IN (1,1,5) DO (
+		FOR /L %%k IN (1,1,5) DO (
+			IF %%i==8 (IF %%j==5 GOTO END_CONV)
+			echo Converting %%i-%%j-%%k.vag
+			audio-converter.exe w %%i %%j %%k
+			CLS
+			echo Converted %%i-%%j-%%k.vag to %%i-%%j-%%k.wav
+			echo.
+			echo ------------------------------
+			echo.
+		)
+	)
+)
+
+:END_CONV
+
+echo All .vag files have been converted.
+echo.
+echo Cleaning up the mess...
+
+DEL *.vag
+DEL *.IMF
+
+echo Good to go!
+echo.
 echo END
